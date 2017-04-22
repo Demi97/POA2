3f3225f812db20f8725d27353ea28e0e2883317e
simonescaboro <simonescaboro@192.168.13.187>
2017-04-22 10:59:20 +0200

    Forse qualcosa funziona

--- CardGame/src/cardgame/cards/Reflexologist.java
+++ CardGame/src/cardgame/cards/Reflexologist.java
@@ -59,7 +59,7 @@ public class Reflexologist implements Card {
         @Override
         public void defend(Creature c) {}
         @Override
-        public int getPower() { return 0; }
+        public int getPower() { return 1; }
         @Override
         public int getToughness() { return 1; }
 
