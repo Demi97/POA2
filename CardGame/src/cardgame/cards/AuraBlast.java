d4eb610f7353a4675dc04defd6cb0637835602dc
simonescaboro <simonescaboro@157.138.168.64>
2017-04-24 11:55:22 +0200

    Miglioramenti carta

---
+++ CardGame/src/cardgame/cards/AuraBlast.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame.cards;
+
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+import cardgame.CardGame;
+import cardgame.Enchantment;
+import java.util.Scanner;
+/**
+ * ciao
+ */
+/**
+ *
+ * @author atorsell
+ */
+public class AuraBlast implements Card {
+    private Player owner2;
+    Scanner reader = new Scanner(System.in);
+    private class AuraBlastEffect extends AbstractCardEffect {
+        public AuraBlastEffect(Player p, Card c) { 
+            super(p,c); 
+            owner2 = CardGame.instance.getAdversary(owner);
+        }
+        
+        
+        public void print_enchantments(Player player) {
+            int i = 0;
+            for(Enchantment e : player.getEnchantments()) {
+                System.out.println((i+1) + ") " + e.name());
+                ++i;
+            }
+        }
+        
+        public int choose_enchantments(Player player) {
+            int choose;
+            do{
+                System.out.println("Choose enchantment: ");
+                choose = reader.nextInt()-1;
+            }while(choose < 0 || choose >= player.getEnchantments().size());
+            
+            return choose;
+        }
+        
+        public void operation(Player player) {
+            int target;
+            if(player.getEnchantments().isEmpty())
+                    System.out.println(player.name() + " has no enchantments on field");
+            else{
+                print_enchantments(player);
+                target = choose_enchantments(player);
+                player.getEnchantments().remove(target);
+            }
+        }
+        
+        public void select_enchantments() {
+            int choose;
+            do{
+                System.out.println(owner.name() + " (1) or " + owner2.name() + " (2)");
+                choose = reader.nextInt()-1;
+            }while(choose != 0&& choose != 1);
+            if(choose == 0)
+                operation(owner);
+            else
+                operation(owner2);
+        }
+            
+        
+        
+        @Override
+        public void resolve() {
+            select_enchantments();
+        }
+    }
+
+    @Override
+    public Effect getEffect(Player owner) { 
+        return new AuraBlastEffect(owner, this); 
+    }
+    
+    @Override
+    public String name() { return "Aura Blast"; }
+    @Override
+    public String type() { return "Instant"; }
+    @Override
+    public String ruleText() { return "Destroy target enchantment. Draw a card"; }
+    @Override
+    public String toString() { return name() + " (" + type() + ") [" + ruleText() +"]";}
+    @Override
+    public boolean isInstant() { return true; }
+}
---
+++ CardGame/src/cardgame/cards/SavorTheMoment.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame.cards;
+
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.CardGame;
+import cardgame.DefaultDrawPhase;
+import cardgame.Effect;
+import cardgame.Player;
+import java.util.Scanner;
+import cardgame.Phases;
+import cardgame.SkipPhase;
+import cardgame.TurnManager;
+/**
+ *
+ * @author simonescaboro
+ */
+public class SavorTheMoment implements Card{
+
+    
+    private class SavorTheMomentEffect extends AbstractCardEffect {
+        public SavorTheMomentEffect(Player p, Card c) { 
+            super(p,c);
+        }
+        
+    @Override
+    public void resolve() {
+        owner.setPhase(Phases.UNTAP, new SkipPhase(Phases.UNTAP));
+        CardGame.instance.setTurnManager(new Extra(owner));
+        }
+    }
+
+    @Override
+    public Effect getEffect(Player owner) { 
+        return new SavorTheMomentEffect(owner, this); 
+    }
+    
+    @Override
+    public String name() { return "Savor The Moment"; }
+    @Override
+    public String type() { return "Sorcery"; }
+    @Override
+    public String ruleText() { return "Take another turn after this one. Skip the untap step of that turn "; }
+    @Override
+    public String toString() { return name() + " (" + type() + ") [" + ruleText() +"]";}
+    @Override
+    public boolean isInstant() { return false; }
+    private class Extra implements TurnManager{
+        private Player player;
+        private Player adversary;
+        
+        public Extra(Player player) {
+            this.player = player;
+            adversary = CardGame.instance.getAdversary(player);
+        }
+        
+        
+        public Player getCurrentPlayer() {
+            return player;
+        }
+        
+        
+        public Player getCurrentAdversary() {
+            return adversary;
+        }
+        
+        
+        public Player nextPlayer() {
+            CardGame.instance.removeTurnManager(this);
+            return player;
+        }
+    }
+    
+    
+}
