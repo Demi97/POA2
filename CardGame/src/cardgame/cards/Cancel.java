5fdc992c2f09bf93a705fad682b9137b645d53c1
Davide Bassetto <biase996@gmail.com>
2017-04-26 15:52:27 +0200

    Cancel - da testare

---
+++ CardGame/src/cardgame/cards/Cancel.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame.cards;
+
+import cardgame.AbstractCardEffect;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+import cardgame.CardGame;
+import cardgame.CardStack;
+import cardgame.MagicPrinter;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Davide
+ */
+public class Cancel implements Card {
+    Scanner reader = new Scanner(System.in);
+    
+    private class CancelEffect extends AbstractCardEffect {
+        Effect target;
+        
+        public CancelEffect(Player p, Card c) { 
+            super(p,c); 
+        }
+        
+        public void selectTarget() {
+            int i = 1;
+            int choose;
+            
+            CardStack stack = CardGame.instance.getStack();
+            for (Effect e : stack) {
+                System.out.println(i + ") " + e.toString());
+                ++i;
+            }
+            
+            do{
+                System.out.println(owner.name() + ": choose target for " + name());
+                choose = reader.nextInt();
+            } while(choose < 1 || choose > i-1);
+            
+            i = 1;
+            for (Effect e : stack) {
+                if(i == choose)
+                    target = e;
+            }
+        }
+        
+        @Override
+        public void resolve() {
+            selectTarget();
+            
+            if (target == null) {
+                System.out.println(card + " has no target");
+            }
+            else {
+                System.out.println(card + " removing " + target + " from stack");
+                CardGame.instance.getStack().remove(target);
+            }
+        }
+    }
+
+    @Override
+    public Effect getEffect(Player owner) { 
+        return new CancelEffect(owner, this); 
+    }
+    
+    @Override
+    public String name() { 
+        return "Cancel"; 
+    }
+    
+    @Override
+    public String type() { 
+        return "Instant"; 
+    }
+    
+    @Override
+    public String ruleText() { 
+        return "Counter target spell"; 
+    }
+    
+    @Override
+    public String toString() { 
+        return name() + " (" + type() + ") [" + ruleText() +"]";
+    }
+    
+    @Override
+    public boolean isInstant() { 
+        return true; 
+    }
+}
