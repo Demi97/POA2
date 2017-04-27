891f37b147a7cc60f43424a209f3679831281c47
diletta <diletta@dileolli-hp>
2017-04-27 09:35:22 +0200

    Sistemata la carta AEther Flash (spero)

--- CardGame/src/cardgame/cards/AEtherFlash.java
+++ CardGame/src/cardgame/cards/AEtherFlash.java
@@ -16,14 +16,13 @@ import cardgame.CardGame;
 import cardgame.Creature;
 import cardgame.TriggerAction;
 import cardgame.Triggers;
-import java.util.List;
 
 /**
  *
  * @author diletta
  */
-public class AEtherFlash implements Card{
-    Creature target1, target2;
+public class AEtherFlash implements Card {
+
     @Override
     public Effect getEffect(Player owner) {
         return new AEtherFlashEffect(owner, this);
@@ -41,16 +40,18 @@ public class AEtherFlash implements Card{
 
     @Override
     public String ruleText() {
-        return "Whenever a creature comes into play"+name()+"deals 2 damage to it";
+        return "Whenever a creature comes into play" + name() + "deals 2 damage to it";
     }
 
     @Override
     public boolean isInstant() {
         return false;
     }
-    private class AEtherFlashEffect extends AbstractEnchantmentCardEffect{
-        public AEtherFlashEffect(Player p, Card c){
-            super(p,c);
+
+    private class AEtherFlashEffect extends AbstractEnchantmentCardEffect {
+
+        public AEtherFlashEffect(Player p, Card c) {
+            super(p, c);
         }
 
         @Override
@@ -58,42 +59,39 @@ public class AEtherFlash implements Card{
             return new AEtherFlashEnchantment(owner);
         }
     }
-    
-    private class AEtherFlashEnchantment extends AbstractEnchantment{
-        public AEtherFlashEnchantment(Player owner){
+
+    private class AEtherFlashEnchantment extends AbstractEnchantment {
+
+        public AEtherFlashEnchantment(Player owner) {
             super(owner);
         }
 
         @Override
         public void insert() {
-            List<Creature> creatures1 = CardGame.instance.getAdversary(owner).getCreatures();
-            List<Creature> creatures2 = owner.getCreatures();
-            target1 = creatures1.get(creatures1.size()-1);
-            target2 = creatures2.get(creatures2.size()-1);
             CardGame.instance.getTriggers().register(Triggers.ENTER_CREATURE_FILTER, DamageOnEntranceAction);
             super.insert();
         }
-        
-        private final TriggerAction DamageOnEntranceAction = new TriggerAction(){
+
+        private final TriggerAction DamageOnEntranceAction = new TriggerAction() {
+
             @Override
             public void execute(Object args) {
-                target1.inflictDamage(2);
-                target2.inflictDamage(2);
+                if (args instanceof Creature) {
+                    ((Creature) args).inflictDamage(2);
+                }
             }
-                
+
         };
-        
-        
-        
+
         @Override
         public String name() {
             return "AEtherFlashEnchantment";
         }
-        
+
         @Override
         public String toString() {
-            return name() + " (" + type() + ") [" + ruleText() +"]";
+            return name() + " (" + type() + ") [" + ruleText() + "]";
         }
-        
+
     }
 }
