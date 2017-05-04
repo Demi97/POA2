3358075b93448da79fa4c14d2a36f468237b1867
diletta <diletta@dileolli-hp>
2017-05-04 10:53:39 +0200

    Iniziata creature Argothian Enchantress

--- CardGame/src/cardgame/cards/ArgothianEnchantress.java
+++ CardGame/src/cardgame/cards/ArgothianEnchantress.java
@@ -5,10 +5,98 @@
  */
 package cardgame.cards;
 
+import cardgame.AbstractCreature;
+import cardgame.AbstractCreatureCardEffect;
+import cardgame.Card;
+import cardgame.CardGame;
+import cardgame.Creature;
+import cardgame.Effect;
+import cardgame.Player;
+import java.util.ArrayList;
+
 /**
  *
  * @author diletta
  */
-public class ArgothianEnchantress {
-    
+public class ArgothianEnchantress implements Card {
+
+    private class ArgothianEnchantressEffect extends AbstractCreatureCardEffect {
+
+        public ArgothianEnchantressEffect(Player p, Card c) {
+            super(p, c);
+        }
+
+        @Override
+        protected Creature createCreature() {
+            return new ArgothianEnchantressCreature(owner);
+        }
+
+    }
+
+    @Override
+    public Effect getEffect(Player owner) {
+        return new ArgothianEnchantressEffect(owner, this);
+    }
+
+    private class ArgothianEnchantressCreature extends AbstractCreature {
+
+        ArrayList<Effect> all_effects = new ArrayList<>();
+        ArrayList<Effect> tap_effects = new ArrayList<>();
+        
+        ArgothianEnchantressCreature(Player owner){
+            super(owner);
+            all_effects.add(new Effect(){
+                @Override
+                public boolean play() {
+                    CardGame.instance.getStack().add(this);
+                    return tap();
+                }
+
+                @Override
+                public void resolve() {
+                    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                }
+                
+            });
+            super.insert();
+            
+        }
+
+        @Override
+        public int getPower() {
+            return 0;
+        }
+
+        @Override
+        public int getToughness() {
+            return 1;
+        }
+
+        @Override
+        public String name() {
+            return "Argorthian Enchantress";
+        }
+
+    }
+
+    @Override
+    public String name() {
+        return "Argothian Enchantress";
+    }
+
+    @Override
+    public String type() {
+        return "Creature";
+    }
+
+    @Override
+    public String ruleText() {
+        return "Shroud (This creature can't be the target of spells or abilities). Whenever you cast an enchantment spell, draw a card";
+    }
+
+    @Override
+    public boolean isInstant() {
+        return false;
+    }
+
 }
--- CardGame/src/cardgame/cards/Reflexologist.java
+++ CardGame/src/cardgame/cards/Reflexologist.java
@@ -20,70 +20,112 @@ import java.util.List;
  * @author atorsell
  */
 public class Reflexologist implements Card {
-    
+
     private class ReflexologistEffect extends AbstractCreatureCardEffect {
-        public ReflexologistEffect(Player p, Card c) { super(p,c); }
-        @Override
-        protected Creature createCreature() { return new ReflexologistCreature(owner); }
-    }
-    @Override
-    public Effect getEffect(Player p) { return new ReflexologistEffect(p,this); }
-    
-    
-    private class ReflexologistCreature extends AbstractCreature {
-        ArrayList<Effect> all_effects= new ArrayList<>();
-        ArrayList<Effect> tap_effects= new ArrayList<>();
-        
-        ReflexologistCreature(Player owner) { 
-            super(owner);
-            all_effects.add( new Effect() { 
-                                    @Override
-                                    public boolean play() { 
-                                        CardGame.instance.getStack().add(this);
-                                        return tap(); 
-                                    }
-                                    @Override
-                                    public void resolve() {}
-                                    @Override
-                                    public String toString() 
-                                        { return "tap: Reflexology does nothing"; }
-                                }
-                ); 
+
+        public ReflexologistEffect(Player p, Card c) {
+            super(p, c);
         }
-        
-        @Override
-        public String name() { return "Reflexologist"; }
-        
-        @Override
-        public void attack(Creature c) {}
-        @Override
-        public void defend(Creature c) {}
-        @Override
-        public int getPower() { return 0; }
-        @Override
-        public int getToughness() { return 1; }
 
         @Override
-        public List<Effect> effects() { return all_effects; }
+        protected Creature createCreature() {
+            return new ReflexologistCreature(owner);
+        }
+    }
+
+    @Override
+    public Effect getEffect(Player p) {
+        return new ReflexologistEffect(p, this);
+    }
+
+    private class ReflexologistCreature extends AbstractCreature {
+
+        ArrayList<Effect> all_effects = new ArrayList<>();
+        ArrayList<Effect> tap_effects = new ArrayList<>();
+
+        ReflexologistCreature(Player owner) {
+            super(owner);
+            all_effects.add(new Effect() {
+                @Override
+                public boolean play() {
+                    CardGame.instance.getStack().add(this);
+                    return tap();
+                }
+
+                @Override
+                public void resolve() {
+                }
+
+                @Override
+                public String toString() {
+                    return "tap: Reflexology does nothing";
+                }
+            }
+            );
+        }
+
         @Override
-        public List<Effect> avaliableEffects() { return (isTapped)?tap_effects:all_effects; }
+        public String name() {
+            return "Reflexologist";
+        }
+
+        @Override
+        public void attack(Creature c) {
+        }
+
+        @Override
+        public void defend(Creature c) {
+        }
+
+        @Override
+        public int getPower() {
+            return 0;
+        }
+
+        @Override
+        public int getToughness() {
+            return 1;
+        }
+
+        @Override
+        public List<Effect> effects() {
+            return all_effects;
+        }
+
+        @Override
+        public List<Effect> avaliableEffects() {
+            return (isTapped) ? tap_effects : all_effects;
+        }
 
         @Override
         public boolean isDefender() {
             return false;
         }
     }
-    
-    
+
     @Override
-    public String name() { return "Reflexologist"; }
+    public String name() {
+        return "Reflexologist";
+    }
+
     @Override
-    public String type() { return "Creature"; }
+    public String type() {
+        return "Creature";
+    }
+
     @Override
-    public String ruleText() { return "Put in play a creature Reflexologist(0/1) with tap: Reflexology does nothing"; }
+    public String ruleText() {
+        return "Put in play a creature Reflexologist(0/1) with tap: Reflexology does nothing";
+    }
+
     @Override
-    public String toString() { return name() + " (" + type() + ") [" + ruleText() +"]";}
+    public String toString() {
+        return name() + " (" + type() + ") [" + ruleText() + "]";
+    }
+
     @Override
-    public boolean isInstant() { return false; }
+    public boolean isInstant() {
+        return false;
+    }
 
 }
